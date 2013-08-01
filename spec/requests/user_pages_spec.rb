f9c4f362b75e4920458ddf8e9c180d72531da91c
PhuongTa <tp.taphuong@gmail.com>
2013-08-01 20:19:46 +0700

    Finish Chapter 7

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,12 @@ describe "User Pages" do
 	 		it "should not create a user" do
 	 			expect {click_button submit}.not_to change(User, :count)
 	 		end
+
+		 	describe "afer submission" do
+		 		before{click_button submit}
+		 		it{should have_title('Sign up')}
+		 		it{should have_content('error')}
+		 	end
 	 	end
 
 	 	describe "with valid information" do
@@ -39,7 +45,16 @@ describe "User Pages" do
 
 	 		it "should create a user" do
 	 			expect {click_button submit}.to change(User, :count).by(1)
+	 		end 		
+
+	 		describe "after saving the user" do
+	 			before {click_button submit}
+	 			let(:user){User.find_by_email(user.email)}
+
+	 			it{should have_title(user.name)}
+	 			it{should have_selector('div.alert.aler-success', text: 'Welcome')}
 	 		end
 	 	end
+
 	 end
 end
