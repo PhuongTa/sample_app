46170d0c5a130e451981ef3802f5b43f116da499
PhuongTa <tp.taphuong@gmail.com>
2013-08-01 18:13:36 +0700

    Signup Form - Create Fail

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  
+  def create
+  	#@user = User.new(user_params) # Not the final implementation!
+    @user = User.new(params[:user])
+  	if @user.save
+  		# Handle a successful save
+  	else
+  		render 'new'
+  	end
   end
+
+  def show
+  	@user = User.find(params[:id])
+  	#render :json => @user.to_json
+  	#return
+  end
+
+  def new
+  	@user = User.new
+  end
+
+  #private
+  #def user_params
+  		#params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  #end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+	factory :user do
+		name "Phoenix Ta"
+		email "phoenixta@ymail.com"
+		password "foobar"
+		password_confirmation "foobar"
+	end
+end
